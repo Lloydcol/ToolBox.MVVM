edd01eeb339f180dae8adc8bef6c553fc5040207
Colart Lloyd <55253106+Lloydcol@users.noreply.github.com>
2021-04-15 17:00:08 +0200

    Adding Mediator pattern

---
+++ Mediator/Messenger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox.MVVM.Mediator
+{
+    public static class Messenger<T>
+    {
+        private static Dictionary<string, Action<T>> _events = new Dictionary<string, Action<T>>();
+
+        public static void Subscribe(string eventName, Action<T> action)
+        {
+            _events.Add(eventName, action);
+        }
+
+        public static void Publish(string eventName, T parameter)
+        {
+            Action<T> action = _events[eventName];
+            action?.Invoke(parameter);
+        }
+    }
+}
