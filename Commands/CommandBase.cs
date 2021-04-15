58555b319155e37a96d76a02e90c153b7f98bbac
Colart Lloyd <55253106+Lloydcol@users.noreply.github.com>
2021-04-15 09:30:40 +0200

    Ajoutez des fichiers projet.

---
+++ Commands/CommandBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ToolBox.MVVM.Commands
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action action;
+        private Func<bool> canActivate;
+
+        public CommandBase(Action action, Func<bool> canActivate = null)
+        {
+            if (action is null) throw new ArgumentException();
+            this.action = action;
+            this.canActivate = canActivate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if(canActivate is null)
+            {
+                return true;
+            }
+            return canActivate.Invoke(); 
+        }
+
+        public void Execute(object parameter)
+        {
+            action?.Invoke();
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, null);
+        }
+    }
+}
